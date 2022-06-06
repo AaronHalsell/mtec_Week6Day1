10be63e0371d70bafc9f2ab54798e40b3bde94e2
Aaron Halsell <aaron.halsell@gmail.com>
2022-06-05 19:30:36 -0600

    created a function that displayed objects listed as inStock true and had a toggle feature for dark mode

---
+++ app.js
@@ -0,0 +1,91 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+$.each(storeItems, function () {
+    if (this.inStock === true) {
+      $("<ul><li>$" + this.price + '</li><li>' +  this.name + '</li><li>' + this.details + '</li></ul>').appendTo('#inStockList').addClass('inStock');
+    }
+  });
+
+
+  $("#toggleButton").on('click', function(){
+    $("#contentContainer").toggleClass('darkMode');
+  });
+
+
+/// The following section is composed of some styles to merely demonstrate I know 
+/// how to add them in with JQuery, I don't believe it's the most efficient way
+/// to add styling though.
+
+$('h1') 
+    .css({
+        'font-size': '42px',
+        'padding': '15px 0'
+    });
+
